6bb437b48c587589d17a0ccba33f1e5d3c825031
moutazmahmoud <72810603+moutazmahmoud@users.noreply.github.com>
2021-04-11 02:36:53 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 
-$(document).ready(function(){
-    $('#hero').slick({
-        // autoplay: true,
-        // autoplaySpeed: 5000,
-        // settings : 'unslick'
-    });
+// toogle burger menu
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav');
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('toggle');
+    nav.classList.toggle('show')
+    
+
   });
+
   
