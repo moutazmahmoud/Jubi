0e138f5cb3c422f48d98c81d28e84163ac554070
moutazmahmoud <72810603+moutazmahmoud@users.noreply.github.com>
2021-04-12 05:35:12 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 // toogle burger menu
+
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav');
   burger.addEventListener('click', () => {
@@ -9,4 +10,34 @@
 
   });
 
-  
+
+// nav Links background color and font color changing
+
+  document.querySelectorAll('#nav-link').forEach((link)=>{
+    link.addEventListener('click', () => {
+      let children1 = link.parentElement.children;
+        for(var i = 0 ; i < children1.length ; i++){
+          children1[i].classList.remove('red-back');
+          children1[i].firstElementChild.classList.remove('white');
+        }
+        link.classList.add('red-back');
+        link.firstElementChild.classList.add('white');
+        nav.classList.remove('show');
+        burger.classList.remove('toggle');
+        
+
+        
+    })
+});
+
+
+// top button - when click => all nav links get to the first style  
+
+document.getElementById('top').addEventListener('click',()=>{
+  const navLinks = document.getElementById('nav-links');
+  console.log(navLinks.children);
+  for(var i = 0 ; i < navLinks.children.length ; i++){
+    navLinks.children[i].classList.remove('red-back');
+    navLinks.children[i].firstElementChild.classList.remove('white');
+  }
+})
